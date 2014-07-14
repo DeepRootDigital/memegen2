aaebe06d9f8f5076adb645c59d3723d9de6f3ff2
Colpanius Jackson <colpanius@gmail.com>
2014-07-14 11:16:21 -0700

    update server

--- server.js
+++ server.js
@@ -20,6 +20,8 @@ var app = express();
 
 // all environments
 app.set('views', __dirname + '/views');
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
 app.engine('html', require('ejs').renderFile);
 app.use(express.favicon());
 app.use(express.logger('dev'));
@@ -130,7 +132,7 @@ smtpTransport.sendMail(mailOptions, function(error, response){
 /* End RESTful actions */
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var serverip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-http.createServer(app).listen(port, serverip, function(){
+http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
   console.log('Express server listening on port ' + port);
 });
 
