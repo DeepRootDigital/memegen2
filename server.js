a38f6c487b993db8d7f2a803d27c8ed54912a70d
Jordan Butler <jordan.butler@comcast.net>
2015-11-24 16:34:16 -0800

    added ways to control media type

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(express.bodyParser({keepExtensions:true,uploadDir:__dirname+'/public/icons/tmp'}));
 app.use(app.router);
-app.use(express.static(__dirname + '/static'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 /* Define all the pages */
